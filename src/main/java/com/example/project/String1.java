fd2dc51566df5613d2b17f300caaf6ae99511478
mansour-9 <126260945+mansour-9@users.noreply.github.com>
2024-09-16 10:59:52 +0300

    Update String1.java

--- src/main/java/com/example/project/String1.java
+++ src/main/java/com/example/project/String1.java
@@ -1,14 +1,14 @@
 package com.example.project;
 
 public class String1 {
-	private char[] str;
-	private int size;
+    private char[] str;
+    private int size;
 
-	public String1 () {
-	   size = -1;
-	   str = new char[80];
-	}
-	public void append(char c) {
+    public String1 () {
+        size = -1;
+        str = new char[80];
+    }
+    public void append(char c) {
         str[++size] = c;
     }
     public char remove() {
@@ -30,9 +30,19 @@ public class String1 {
             append(s.getChar(i));
     }
     public boolean equal(String1 s) {
-        throw new UnsupportedOperationException("equal() function is not supported yet.");
+       boolean x=false;
+       for(int i= 0 ;i<size;i++){
+           if (getChar(i) == s.getChar(i))
+               return true;
+       }
+       return false;
     }
     public void reverse() {
-        throw new UnsupportedOperationException("reverse() function is not supported yet.");
+        int n = this.length();
+        for (int i = 0; i < n / 2; i++) {
+            char temp = str[i];
+            str[i] = str[n - i - 1];
+            str[n - i - 1] = temp;
+        }
     }
 }
